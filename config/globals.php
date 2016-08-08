f40bd43023c2a4c66a5ed8047b14d3f87c8a0d0b
Nageswara Rao <nag.samayam@gmail.com>
2016-08-08 21:17:09 +0530

    Home page added

---
+++ config/globals.php
@@ -0,0 +1,5 @@
+<?php
+return [ 
+		'site_title' => 'Product Manager University',
+		'site_title_abbr' => 'PMU' 
+];
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var elixir = require('laravel-elixir');
 
 /*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
+ * |-------------------------------------------------------------------------- |
+ * Elixir Asset Management
+ * |-------------------------------------------------------------------------- | |
+ * Elixir provides a clean, fluent API for defining some basic Gulp tasks | for
+ * your Laravel application. By default, we are compiling the Sass | file for
+ * our application, as well as publishing vendor resources. |
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	// mix.sass('app.scss');
+	mix.styles([ 'bootstrap.min.css', 'style.css' ], 'public/assets/css')
+			.scripts([ 'jquery.min.js', 'bootstrap.min.js' ],
+					'public/assets/js');
+
+	mix.version([ 'assets/css/all.css', 'assets/js/all.js', 'assets/images' ]);
+	mix.copy('resources/assets/images', 'public/build/assets/images');
+	mix.copy('resources/assets/fonts', 'public/build/assets/fonts');
 });
